1bc46bd6da8dd4173729bb34824b56993d91990d
eestherr088 <es9067688@gmail.com>
2025-09-24 09:33:45 -0300

    24.09.2025

---
+++ firebase/cine_favorite/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  //garantir o carregamanetos do widgets primeiro
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //conectar com o firebase
+  await Firebase.initializeApp();
+
+  runApp(
+    MaterialApp(
+      title: "Cine Favorite",
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        brightness: Brightness.dark,
+      ),
+      home:
+          AuthStream(), // permite a navegação de tela de acordo com alguma decisão
+    ),
+  );
+}
+
+class AuthStream extends StatelessWidget {
+  const AuthStream({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ouvinte das mudanças de estado (listener)
+    return StreamBuilder<User?>(
+      //retorna o status do Usuário (null/logado)
+      stream: FirebaseAuth.instance
+          .authStateChanges(), //identifica a mundaça de status do usuário
+      builder: (context, snapshot) {
+        // analisa o instantâneo da aplicação
+        //se tiver logado, vai para a tela de favoritos
+        if (snapshot.hasData) {
+          return FavoriteView();
+        } //caso contrário => tela de login
+        return LoginView();
+      },
+    );
+  }
+}
